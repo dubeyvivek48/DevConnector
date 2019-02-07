287fffb405bb8a012386cf36a770cfa6a3d0098e
dubeyvivek48 <dubeyvivek48@gmail.com>
2019-02-07 23:54:37 +0530

    routes added

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const users = require('./routes/api/users');
+const profile  = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
 const app= express();
 // DB Config
 const db = require('./config/keys').mongoURI;
@@ -10,6 +14,14 @@ mongoose
   .connect(db)
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
+
+  // use routes
+  app.use('/api/users',users);
+  app.use('/api/profile',profile);
+  app.use('/api/posts',posts);
+  
+
+
 
 app.get('/', (req, res) => res.send('Hello World'));
 const port=process.env.PORT || 5000;
